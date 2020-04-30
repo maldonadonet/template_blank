196c037ce753328cc70e7b44e996b1c0d3feb5bd
Ruben <desarrollo@ail.com.mx>
2020-04-29 22:24:58 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,24 +7,37 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+// Firebase
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireDatabaseModule,AngularFireDatabase,} from "@angular/fire/database";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBqTlsjVvUTvBSOqq2DqwR12IzA_MR1AXA",
+  authDomain: "fir-ionic-c7779.firebaseapp.com",
+  databaseURL: "https://fir-ionic-c7779.firebaseio.com",
+  projectId: "fir-ionic-c7779",
+  storageBucket: "fir-ionic-c7779.appspot.com",
+  messagingSenderId: "999120183",
+  appId: "1:999120183:web:3fbcdc3d9f4f6bbb9f0eb2",
+};
+
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
+  declarations: [MyApp, HomePage],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    AngularFireDatabase,
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+  ],
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireDatabase } from "@angular/fire/database";
+import { Observable } from "rxjs/Observable";
+
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html",
 })
 export class HomePage {
+  
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, afDB: AngularFireDatabase) {
+    
   }
-
 }
